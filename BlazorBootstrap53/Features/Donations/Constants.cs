6cbdb548375bbef701dd6cca09b0bd00f3369bc4
John Marsing <JohnMarsing@outlook.com>
2024-05-05 22:45:49 -0700

    Finished upgraded to .Net 8

--- BlazorBootstrap53/Features/Donations/Constants.cs
+++ BlazorBootstrap53/Features/Donations/Constants.cs
@@ -4,4 +4,4 @@ public static class Constants
 {
 	public const string QRC_Image = "qr_donation_14kfZN4jYc0r1cQ4gh.jpg";
 }
-// Ignore Spelling: QRC
+// Ignore Spelling: QRC@using BlazorBootstrap53.Features.Home
